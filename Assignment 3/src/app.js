387747d2af19c2641706f2b04a2d346a37800070
Quang Nguyen <Kwa@Quangs-MacBook-Pro.local>
2016-03-09 14:14:12 +0000

    asd

--- Assignment 3/src/app.js
+++ Assignment 3/src/app.js
@@ -5,7 +5,15 @@ angular.module("project3App", ["ngRoute", "ui.bootstrap", "sharedServices", "pas
 	$routeProvider.when("/", {
 		controller: "SellersController",
 		templateUrl: "components/sellers/sellers.html"
+	})
+	.when("#/seller/:id", {
+		controller: "SellerDetailsController.js",
+		templateUrl: "components/seller-details/seller-details.html"
+	})
+	.otherwise ({
+		templateUrl: "components/shared/notFound.html"
 	});
+
 
 	$translateProvider.useStaticFilesLoader({
 		prefix: "lang_",
