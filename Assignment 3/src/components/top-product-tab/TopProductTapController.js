e9dbbdd061c4fb8878ee3dbd11288bcb480d3f1e
Ásgeir Frímannsson <Asgeir@clients-pool-1.nat.ru.is>
2016-03-11 15:45:45 +0000

    git dat ass

--- Assignment 3/src/components/top-product-tab/TopProductTapController.js
+++ Assignment 3/src/components/top-product-tab/TopProductTapController.js
@@ -7,12 +7,34 @@ function TopProductTapController($scope, AppResource, centrisNotify, ProductDlg,
 	var sellerID = parseInt($scope.id);
 
 	AppResource.getSellerProducts(sellerID).success(function(sellerProducts) {
-		$scope.sellerProducts = sellerProducts;
+		sellerProducts.sort(compare);
+		$scope.sellerProducts = sellerProducts.slice(0, 10);
 	});
 
-	$scope.sellerProducts = $scope.sellerProducts.slice(0, 10);
+	//For sort by quantities sold
+	function compare(a,b) {
+  		if (a.quantitySold > b.quantitySold) {
+    		return -1;
+  		}
+  		else if (a.quantitySold  < b.quantitySold) {
+    		return 1;
+  		}
+  		else {
+    		return 0;
+  		}
+	}
 
 	$scope.onAddProduct = function onAddProduct() {
-		console.log("top");
+		ProductDlg.show().then(function(product) {
+			AppResource.addSellerProduct(sellerID, product).success(function() {
+				AppResource.getSellerProducts(sellerID).success(function(sellerProducts) {
+					sellerProducts.sort(compare);
+					$scope.sellerProducts = sellerProducts.slice(0, 10);
+				});
+				centrisNotify.success("sellers.Messages.SaveSucceeded", "sellers.Ok");
+			}).error(function() {
+				centrisNotify.error("sellers.Messages.SaveFailed", "sellers.Fail");
+			});
+		});
 	};
 });
